4268a58273caa72cd785e13c4dc0c99b093c5404
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-10-30 22:40:21 +0530

    routers for auth

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,22 @@
+// !    sign up
+export const signUp = async (req, res) => {
+    res.json({
+        data: 'listning to signup'
+    })
+}
+
+
+// !    login
+export const login = async (req, res) => {
+    res.json({
+        data: 'listning to login'
+    })
+}
+
+
+// !    logout
+export const logout = async (req, res) => {
+    res.json({
+        data: 'listning to logout'
+    })
+}
---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routers/auth.router.js";
+
+//env
+dotenv.config();
+
+//app
+const app = express();
+
+//use routers
+app.use('/api/v2/auth', router);
+
+//port
+const port = process.env.PORT || 5000;
+
+// middleware for accept json
+app.use(express.json());
+
+//listen
+app.listen(port, () => {
+    console.log(`app is listning at port : ${port}`)
+});
